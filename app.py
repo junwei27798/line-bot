d5c24db920b3749df1331d8211776a820d7775c5
junwei27798 <93516848+junwei27798@users.noreply.github.com>
2026-02-25 09:46:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -57,4 +58,5 @@ def handle_message(event):
     )
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # 取得 Render 指定的 PORT
+    app.run(host="0.0.0.0", port=port)
